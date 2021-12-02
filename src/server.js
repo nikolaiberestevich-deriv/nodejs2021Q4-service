86cc1161d9d06bb9cb55f0c6f6238448774899fe
Nikolai Berestevich (Mikalai Berastsevich) <nikolai.berestevich@gmail.com>
2021-12-02 06:03:51 +0300

    inut: dev branch

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const { PORT } = require('./common/config');
 const app = require('./app');
-
+// for 1st commit
 app.listen(PORT, () =>
   console.log(`App is running on http://localhost:${PORT}`)
 );
